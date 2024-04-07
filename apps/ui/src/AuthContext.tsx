1d6de60ec35d7210cb444af1093697a469d04871
Ravshan Samandarov <ravshansbox@gmail.com>
2024-04-07 18:45:02 +0300

    Implement AuthContext

--- apps/ui/src/AuthContext.tsx
+++ apps/ui/src/AuthContext.tsx
@@ -5,21 +5,90 @@ import {
   type FC,
   type PropsWithChildren,
 } from 'react';
+import { trpcVanillaClient } from './trpc';
 
-type ContextValue = {
-  authorized: boolean;
+type ContextState = {
+  isRestoreTriggered: boolean;
+  isRestoring: boolean;
+  isAuthenticating: boolean;
+  isAuthenticated: boolean;
+  tokenId: string | null;
+  username: string | null;
 };
 
-const AuthContext = createContext<ContextValue>({
-  authorized: false,
+const initialValue: ContextState = {
+  isRestoreTriggered: false,
+  isRestoring: false,
+  isAuthenticating: false,
+  isAuthenticated: false,
+  tokenId: null,
+  username: null,
+};
+
+type Credentials = {
+  username: string;
+  password: string;
+};
+
+type ContextType = {
+  state: ContextState;
+  restore: () => Promise<void>;
+  login: (credentials: Credentials) => Promise<void>;
+  logout: () => Promise<void>;
+};
+
+const throwError = async () => {
+  throw new Error(`Cannot be called outside of a Context Provider`);
+};
+
+const AuthContext = createContext<ContextType>({
+  state: initialValue,
+  restore: throwError,
+  login: throwError,
+  logout: throwError,
 });
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [value] = useState<ContextValue>({
-    authorized: false,
-  });
+  const [state, setState] = useState<ContextState>(initialValue);
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  const update = (partial: Partial<ContextState>) => {
+    setState((value) => ({ ...value, ...partial }));
+  };
+
+  const restore = async () => {
+    update({ isRestoreTriggered: true });
+    const tokenId = window.localStorage.getItem('TOKEN');
+    if (tokenId === null) {
+      throw new Error('No token');
+    }
+    update({ isRestoring: true });
+    try {
+      const accessToken = await trpcVanillaClient.tokens.fetchToken.query({
+        id: tokenId,
+      });
+      update({ tokenId, username: accessToken.user.username });
+    } catch (error) {
+      window.localStorage.removeItem('TOKEN');
+      throw error;
+    } finally {
+      update({ isRestoring: false });
+    }
+  };
+
+  const login = async (credentials: Credentials) => {};
+
+  const logout = async () => {
+    if (!state.tokenId) return;
+    await trpcVanillaClient.tokens.deleteToken.mutate({ id: state.tokenId });
+    window.localStorage.removeItem('TOKEN');
+    setState({ ...initialValue, isRestoreTriggered: true });
+  };
+
+  return (
+    <AuthContext.Provider value={{ state, restore, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- apps/ui/src/trpc.ts
+++ apps/ui/src/trpc.ts
@@ -1,12 +1,18 @@
 import type { AppRouter } from '@cloudretail/api';
 import { QueryClient } from '@tanstack/react-query';
-import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
+import {
+  createTRPCClient,
+  createTRPCReact,
+  httpBatchLink,
+} from '@trpc/react-query';
 import { VITE_API_BASE_URL } from './constants';
 
 export const trpc = createTRPCReact<AppRouter>();
 
-export const trpcClient = trpc.createClient({
-  links: [httpBatchLink({ url: VITE_API_BASE_URL })],
-});
+const link = httpBatchLink({ url: VITE_API_BASE_URL });
+
+export const trpcVanillaClient = createTRPCClient<AppRouter>({ links: [link] });
+
+export const trpcClient = trpc.createClient({ links: [link] });
 
 export const queryClient = new QueryClient();
